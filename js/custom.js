8ae97e0e942c946f3f272ed5ac789e6192a61049
kimhye1105 <kimhyein1105@gmail.com>
2021-08-06 14:31:46 +0900

    아이소톡 수정

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+    var $grid; 
+
+    $grid = $(".wrap").isotope({ //모션을 적용할 박스의 부모 선택자명 
+        itemSelector:".wrap article", //모션을 적용할 박스 선택자명 
+        columnWidth:".wrap article", //너비값을 구할 박스 선택자명 
+        transitionDuration:"0.8s", //모션 이동 속도 
+        percentPosition:true //너비값이 퍼센트 :true, 고정픽셀:false 
+    });
+
+    $(".filter li a").on("click", function(e){
+        e.preventDefault();
+
+        var sort = $(this).attr("href");
+        
+        $grid.isotope({
+            filter:sort
+        });
+
+        $(".filter li a").removeClass("on");
+        $(this).addClass("on");
+    });
+
+
+/*
+    $(선택자).on("event", function(){ event: click, mouseenter, mouselave, scroll
+
+    });
+*/
+
+});
